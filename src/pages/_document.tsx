bc60c4b2de8897a19c985a98cf0781a441807ab3
Fernando González Tostado <68973307+esponges@users.noreply.github.com>
2024-02-20 15:51:00 -0600

    setup shadcn

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,13 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document';
+
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang='en'>
       <Head />
-      <body>
+      <body
+        
+      >
         <Main />
         <NextScript />
       </body>
